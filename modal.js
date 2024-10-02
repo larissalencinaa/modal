d49eaaf126b87348ddc2db95f3f6efa7145086ce
larissalencinaa <162700596+larissalencinaa@users.noreply.github.com>
2024-10-01 22:40:05 -0300

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,16 @@
+function abrirModal(){
+
+    let modal = document.getElementById('janela-modal')
+
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal'){
+
+            modal.classList.remove('abrir')
+            
+        }
+
+    })
+}
